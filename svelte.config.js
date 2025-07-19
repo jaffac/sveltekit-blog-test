bfe62b84c19c69c6000af224ca72795f88bb1561
jaffac <jaffaster@gmail.com>
2025-07-19 14:13:59 +0200

    try

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
 import adapter from '@sveltejs/adapter-cloudflare'
-import adapter from '@sveltejs/adapter-vercel'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 import { mdsvex, escapeSvelte } from 'mdsvex'
