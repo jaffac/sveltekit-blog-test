5219d66b15250ce2294bd5fc39d099231e10856d
jaffac <jaffaster@gmail.com>
2026-01-21 21:03:16 +0100

    newDomain

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,4 +3,4 @@ import { dev } from '$app/environment'
 export const title = 'Blog.link'
 export const description = 'Self-hosting, privacy, and lightweight dev from Sarajevo.'
 // This ensures your RSS feed and social tags use the real URL when live
-export const url = dev ? 'http://localhost:5173' : 'https://blog.jaffac.link'
+export const url = dev ? 'http://localhost:5173' : 'https://blog.jaffac.dpdns.org'
